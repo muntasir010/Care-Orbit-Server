82b7ab9c7d450baac71699726af212d476940209
muntasir010 <naeemtasir03@gmail.com>
2026-03-11 00:06:22 +0600

    feat: Added meta data on get all users function

--- src/app/modules/user/user.constants.ts
+++ src/app/modules/user/user.constants.ts
@@ -1,1 +1,3 @@
 export const userSearchableFields =["email"];
+export const userFilterableFields = ["status", "role", "email", "searchTerm"];
+export const userOptionAbleFields = ["page", "limit", "searchTerm", "sortBy", "sortOrder"];
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,18 +1,20 @@
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import pick from "../../helper/pick";
+import { userFilterableFields, userOptionAbleFields } from "./user.constants";
 import { UserService } from "./user.service";
 
 const getAllUsers = catchAsync(async(req, res) =>{
-  const filters = pick(req.query, ["status", "role", "email", "searchTerm"]);
-  const options = pick(req.query, ["page", "limit", "searchTerm", "sortBy", "sortOrder", ])
+  const filters = pick(req.query, userFilterableFields);
+  const options = pick(req.query, userOptionAbleFields)
    
   const result = await UserService.getAllUsers( filters, options);
   sendResponse(res, {
     statusCode: 200,
     success: true,
     message: "Users retrieved successfully",
-    data: result,
+    meta: result.meta,
+    data: result.data
   });
 })
 
