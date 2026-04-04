cab93bc4758e5c0785770b953d2a5a1fd01db38d
muntasir010 <naeemtasir03@gmail.com>
2026-04-04 23:48:45 +0600

    refeat: doctor schedule filters, sort and paginate successully

--- src/app/modules/doctorSchedules/doctorSchedules.controller.ts
+++ src/app/modules/doctorSchedules/doctorSchedules.controller.ts
@@ -1,36 +1,43 @@
-import httpStatus from 'http-status';
-import catchAsync from "../../../shared/catchAsync"
+import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
-import { DoctorSchedulesServices } from './doctorSchedules.service';
-import type { Request, Response } from 'express';
-import type { IAuthUser } from '../../interfaces/common';
+import { DoctorSchedulesServices } from "./doctorSchedules.service";
+import type { Request, Response } from "express";
+import type { IAuthUser } from "../../interfaces/common";
+import pick from "../../interfaces/pick";
 
-const insertIntoDB = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
-
+const insertIntoDB = catchAsync(
+  async (req: Request & { user?: IAuthUser }, res: Response) => {
     const user = req.user;
     const result = await DoctorSchedulesServices.insertIntoDB(user, req.body);
 
     sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Doctor Schedule created successfully!",
-        data: result
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Doctor Schedule created successfully!",
+      data: result,
     });
+  },
+);
+
+const schedulesForDoctor = catchAsync(async (req, res) => {
+  const filters = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+  const options = pick(req.query, ["startDateTime", "endDateTime"]);
+
+  const result = await DoctorSchedulesServices.schedulesForDoctor(
+    filters,
+    options,
+  );
+
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Schedules Fetch Successfully!",
+    data: result,
+  });
 });
 
-const schedulesForDoctor = catchAsync(async(req, res)=>{
-    const result = await DoctorSchedulesServices.schedulesForDoctor();
-
-    sendResponse(res,{
-        statusCode: 201,
-        success: true,
-        message: "Schedules Fetch Successfully!",
-        data: result
-    })
-})
-
-
 export const DoctorScheduleControllers = {
-    insertIntoDB,
-    schedulesForDoctor,
-}
+  insertIntoDB,
+  schedulesForDoctor,
+};
--- src/app/modules/doctorSchedules/doctorSchedules.service.ts
+++ src/app/modules/doctorSchedules/doctorSchedules.service.ts
@@ -1,5 +1,10 @@
 import { addHours, addMinutes, format } from "date-fns";
 import prisma from "../../../shared/prisma";
+import {
+  paginationHelper,
+  type IOptions,
+} from "../../interfaces/paginationHelper";
+import type { Prisma } from "@prisma/client";
 
 const insertIntoDB = async (payload: any, body: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
@@ -55,15 +60,66 @@ const insertIntoDB = async (payload: any, body: any) => {
         slotStartDateTime.getMinutes() + intervalTime,
       );
     }
-    currentDate.setDate(currentDate.getDate()+1);
+    currentDate.setDate(currentDate.getDate() + 1);
   }
 
   return schedules;
-};  
+};
 
-const schedulesForDoctor = async()=>{
+const schedulesForDoctor = async (filters: any, options: IOptions) => {
+  const { skip, limit, page, sortBy, sortOrder } =
+    paginationHelper.calculatePagination(options);
+  const { startDateTime: filterStartDateTime, endDateTime: filterEndDateTime } =
+    filters;
 
-}
+  const andConditions: Prisma.ScheduleWhereInput[] = [];
+
+  if (filterStartDateTime & filterEndDateTime) {
+    andConditions.push({
+      AND: [
+        {
+          startDateTime: {
+            gte: filterStartDateTime,
+          },
+        },
+        {
+          endDateTime: {
+            lte: filterEndDateTime,
+          },
+        },
+      ],
+    });
+  }
+
+  const whereConditions: Prisma.ScheduleWhereInput =
+    andConditions.length > 0
+      ? {
+          AND: andConditions,
+        }
+      : {};
+
+  const result = await prisma.schedule.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+  });
+
+  const total = await prisma.schedule.count({
+    where: whereConditions
+  })
+
+  return {
+    meta: {
+      page,
+      limit,
+      total
+    },
+    data: result
+  }
+};
 
 export const DoctorSchedulesServices = {
   insertIntoDB,
