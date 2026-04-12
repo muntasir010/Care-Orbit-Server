bfeab7c7ee00a7639c784569c8d7e2ec144f3083
muntasir010 <naeemtasir03@gmail.com>
2026-04-12 23:34:45 +0600

    feat: handle Prisma unknown and initialization errors with user-friendly messages and status cdoe

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import { Prisma } from "@prisma/client";
 import type { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
 const globalErrorHandler = (
   err: any,
@@ -15,19 +16,33 @@ const globalErrorHandler = (
       ((message =
         "This record already exists. Please try with different data."),
         (err = err.meta));
+      statusCode = httpStatus.CONFLICT;
     }
     if ((err.code = "P1000")) {
       ((message = "Database connection issue. Please try again later."),
         (err = err.meta));
+      statusCode = httpStatus.BAD_GATEWAY;
     }
     if ((err.code = "P2003")) {
       ((message =
         "This data is linked with another record and cannot be changed."),
         (err = err.meta));
+      statusCode = httpStatus.BAD_REQUEST;
     }
   } else if (err instanceof Prisma.PrismaClientValidationError) {
-    message = "The data format is incorrect or some required fields are missing.";
+    message =
+      "The data format is incorrect or some required fields are missing.";
     err = err.message;
+    statusCode = httpStatus.BAD_REQUEST;
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    message = "An unexpected error occurred. Please try again later.";
+    err = err.message;
+    statusCode = httpStatus.BAD_REQUEST;
+  }else if(err instanceof Prisma.PrismaClientInitializationError){
+    message = "Could not connect to the database. Please check your internet or server status."
+    err= err.message;
+    statusCode = httpStatus.BAD_REQUEST;
+
   }
 
   return res.status(statusCode).json({
