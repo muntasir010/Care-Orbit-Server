acec9e30ea874e4da36cb16021a8a72d024bf781
muntasir010 <naeemtasir03@gmail.com>
2026-07-10 00:52:40 +0600

    feat(meta): admin meta data get successfully

--- src/app/modules/meta/meta.service.ts
+++ src/app/modules/meta/meta.service.ts
@@ -1,13 +1,14 @@
 import httpStatus from "http-status";
-import { UserRole } from "@prisma/client";
+import { PaymentStatus, UserRole } from "@prisma/client";
 import type { IAuthUser } from "../../interfaces/common";
 import AppError from "../../errors/AppError";
+import prisma from "../../shared/prisma";
 
 const fetchDashboardMetaData = async (user: IAuthUser) => {
   let metadata;
   switch (user?.role) {
     case UserRole.ADMIN:
-      metadata = "Admin Metadata";
+      metadata = await getAdminMetaData();
       break;
 
     case UserRole.DOCTOR:
@@ -22,6 +23,64 @@ const fetchDashboardMetaData = async (user: IAuthUser) => {
   }
 
   return metadata;
+};
+
+const getAdminMetaData = async () => {
+  const patientCount = await prisma.patient.count();
+  const doctorCount = await prisma.doctor.count();
+  const adminCount = await prisma.admin.count();
+  const appointmentCount = await prisma.appointment.count();
+  const paymentCount = await prisma.payment.count();
+
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      amount: true,
+    },
+    where: {
+      status: PaymentStatus.PAID,
+    },
+  });
+
+  const barChartData = await getBarChartData();
+  const pieChartData = await getPieChartData();
+
+  return {
+    patientCount,
+    doctorCount,
+    adminCount,
+    appointmentCount,
+    paymentCount,
+    totalRevenue,
+    barChartData,
+    pieChartData,
+  };
+};
+
+const getBarChartData = async () => {
+  const appointmentCountPerMonth = await prisma.$queryRaw`
+    SELECT DATE_TRUNC("month", "createdAt") AS month
+    CAST(COUNT (*) AS INTEGER) AS count
+    FROM "appointments"
+    GROUP BY month
+    ORDER BY month ASC
+    `;
+
+  return appointmentCountPerMonth;
+};
+
+const getPieChartData = async () => {
+  const appointmentStatusDistribution = await prisma.appointment.groupBy({
+    by: ["status"],
+    _count: { id: true },
+  });
+
+  const formattedAppointmentStatusDistribution =
+    appointmentStatusDistribution.map(({ status, _count }) => {
+      status;
+      Number(_count.id);
+    });
+
+  return formattedAppointmentStatusDistribution;
 };
 
 export const MetaService = {
