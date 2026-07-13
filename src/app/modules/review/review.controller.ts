68287d05cb1063d7e8eaa3dbb53f0693db660c74
muntasir010 <naeemtasir03@gmail.com>
2026-07-13 18:48:34 +0600

    feat(review): get all review with pagination and filtering successfully

--- src/app/modules/review/review.controller.ts
+++ src/app/modules/review/review.controller.ts
@@ -4,6 +4,8 @@ import type { IAuthUser } from "../../interfaces/common";
 import catchAsync from "../../shared/catchAsync";
 import { ReviewService } from "./review.service";
 import sendResponse from "../../shared/sendResponse";
+import pick from "../../interfaces/pick";
+import { reviewFilterableFields } from "./review.constant";
 
 const insertIntoDB = catchAsync(
   async (req: Request & { user?: IAuthUser }, res: Response) => {
@@ -19,6 +21,20 @@ const insertIntoDB = catchAsync(
   },
 );
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, reviewFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await ReviewService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Reviews retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
 export const ReviewController = {
   insertIntoDB,
+  getAllFromDB
 };
--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -2,6 +2,8 @@ import httpStatus from "http-status";
 import AppError from "../../errors/AppError";
 import type { IAuthUser } from "../../interfaces/common";
 import prisma from "../../shared/prisma";
+import type { Prisma } from "@prisma/client";
+import { paginationHelper, type IPaginationOptions } from "../../interfaces/paginationHelper";
 
 const insertIntoDB = async (user: IAuthUser, payload: any) => {
   const patientData = await prisma.patient.findUniqueOrThrow({
@@ -56,6 +58,64 @@ const insertIntoDB = async (user: IAuthUser, payload: any) => {
   });
 };
 
+const getAllFromDB = async (
+    filters: any,
+    options: IPaginationOptions,
+) => {
+    const { limit, page, skip } = paginationHelper.calculatePagination(options);
+    const { patientEmail, doctorEmail } = filters;
+    const andConditions = [];
+
+    if (patientEmail) {
+        andConditions.push({
+            patient: {
+                email: patientEmail
+            }
+        })
+    }
+
+    if (doctorEmail) {
+        andConditions.push({
+            doctor: {
+                email: doctorEmail
+            }
+        })
+    }
+
+    const whereConditions: Prisma.ReviewWhereInput =
+        andConditions.length > 0 ? { AND: andConditions } : {};
+
+    const result = await prisma.review.findMany({
+        where: whereConditions,
+        skip,
+        take: limit,
+        orderBy:
+            options.sortBy && options.sortOrder
+                ? { [options.sortBy]: options.sortOrder }
+                : {
+                    createdAt: 'desc',
+                },
+        include: {
+            doctor: true,
+            patient: true,
+            appointment: true,
+        },
+    });
+    const total = await prisma.review.count({
+        where: whereConditions,
+    });
+
+    return {
+        meta: {
+            total,
+            page,
+            limit,
+        },
+        data: result,
+    };
+};
+
 export const ReviewService = {
   insertIntoDB,
+  getAllFromDB
 };
